c0a712eccb2d7c1111f97a925dc026e5c6202562
Zxr1un <zakharpodstud@gmail.com>
2025-09-22 14:30:13 +0300

    Zxr task ready, read README

--- PsychoAT/Program.cs
+++ PsychoAT/Program.cs
@@ -10,57 +10,53 @@ using static System.Net.WebRequestMethods;
 
 namespace PsychoAT
 {
-    
+
     /*Классы для хранения тестов*/
-    
-        //Test storage
-        public class Psycho_Test
-        {
-            //здесь будет 2 конструктора
-            //для списка из тестов
-            public Psycho_Test(int id, string title)
-            {
-                this.id = id;
-                this.name = title;
-            }
+
+    //Test storage
+    public class Psycho_Test
+    {
+
         //для конкретного теста
-            public Psycho_Test(int id, string title,string type, string author)
-            {
-                this.id = id;
-                this.name = title;
-            }
-
-            public int id = -1; //id в БД
-            public string name = "none"; // Название
-            public string type = "none"; // Тип
-            public string author = "none"; // Автор
-            public int amm_of_questions = 0; // Количество вопросов в тесте
-
-            public List<Question> questions = new List<Question>(0); //список вопросов
-
-        }
-        //Question storage
-        public class Question
+        public Psycho_Test(int id, string title, string type = "none", string author = "none")
         {
-            public int id = -1;
-            public string text = "none";
-            public List<Answer> answers = new List<Answer>(0);
+            this.id = id;
+            this.name = title;
+            this.type = type;
+            this.author = author;
+        }
 
-        }
-        //Answer storage
-        public class Answer
-        {
-            public int id = -1;
-            public string text = "none";
-            public List<Points_cods> points_cods = new List<Points_cods>();
-        }
-        //Storage of type and value of points of current answer
-        public class Points_cods
-        {
-            public int id = -1;
-            public string type = "none";
-            public int value = -1001;
-        }
+        public int id = -1; //id в БД
+        public string name = "none"; // Название
+        public string type = "none"; // Тип
+        public string author = "none"; // Автор
+        public int amm_of_questions = 0; // Количество вопросов в тесте
+
+        public List<Question> questions = new List<Question>(0); //список вопросов
+
+    }
+    //Question storage
+    public class Question
+    {
+        public int id = -1;
+        public string text = "none";
+        public List<Answer> answers = new List<Answer>(0);
+
+    }
+    //Answer storage
+    public class Answer
+    {
+        public int id = -1;
+        public string text = "none";
+        public List<Points_cods> points_cods = new List<Points_cods>();
+    }
+    //Storage of type and value of points of current answer
+    public class Points_cods
+    {
+        public int id = -1;
+        public string type = "none";
+        public int value = -1001;
+    }
 
 
 
@@ -90,10 +86,10 @@ namespace PsychoAT
             connectionString = $"Data Source={dbPath};Version={version};";
             MessageBox.Show(connectionString);
         }
-        
+
         public void load_all_tests()
         {
-           
+
             using (SQLiteConnection conn = new SQLiteConnection(connectionString))
             {
                 conn.Open();
@@ -103,10 +99,20 @@ namespace PsychoAT
                 {
                     while (reader.Read())
                     {
-                        
-                        int id = Convert.ToInt32(reader["id"]);
+
+
+                        int testId = Convert.ToInt32(reader["id"]);
                         string title = reader["title"].ToString();
-                        tests.Add(new Psycho_Test(id, title));
+                        string type = reader["type"].ToString();
+                        string author = reader["author"].ToString();
+
+                        tests.Add(new Psycho_Test(testId, title, type, author));
+
+                        using (SQLiteCommand countCmd = new SQLiteCommand("SELECT COUNT(*) FROM questions WHERE test_id = @id", conn))
+                        {
+                            countCmd.Parameters.AddWithValue("@id", testId);
+                            tests[tests.Count - 1].amm_of_questions = Convert.ToInt32(countCmd.ExecuteScalar());
+                        }
                     }
                 }
             }
@@ -127,11 +133,16 @@ namespace PsychoAT
                         {
                             int testId = Convert.ToInt32(reader["id"]);
                             string title = reader["title"].ToString();
-                            //string type = reader["type"].ToString();
-                            //string author = reader["author"].ToString();
+                            string type = reader["type"].ToString();
+                            string author = reader["author"].ToString();
+                            current_test = new Psycho_Test(testId, title, type, author);
 
-                            // возвращаем объект
-                            current_test = new Psycho_Test(testId, title);
+                            using (SQLiteCommand countCmd = new SQLiteCommand("SELECT COUNT(*) FROM questions WHERE test_id = @id", conn))
+                            {
+                                countCmd.Parameters.AddWithValue("@id", testId);
+                                current_test.amm_of_questions = Convert.ToInt32(countCmd.ExecuteScalar());
+                            }
+
                             return;
                         }
                     }
@@ -146,18 +157,18 @@ namespace PsychoAT
             string output = "";
             foreach (Psycho_Test a in tests)
             {
-                output += a.id.ToString() + " | " + a.name + "\n";
+                output += a.id.ToString() + " | " + a.name + " | " + current_test.type + " | " + current_test.author + "| вопросов: " + a.amm_of_questions + "\n";
             }
             MessageBox.Show(output);
         }
         public void show_current_test()
         {
-            if(current_test == null)
+            if (current_test == null)
             {
                 MessageBox.Show("NULL");
                 return;
             }
-            string output = current_test.id.ToString() + " | " + current_test.name + "\n";
+            string output = current_test.id.ToString() + " | " + current_test.name + " | " + current_test.type + " | " + current_test.author + "| вопросов: " + current_test.amm_of_questions + "\n";
             MessageBox.Show(output);
         }
 
@@ -167,22 +178,22 @@ namespace PsychoAT
         }
     }
 
-    
+
 
 
 
     internal static class Program
     {
         public static DB_work db = new DB_work();
-        
+
 
         public class Current_test
         {
 
         }
 
-        public static  int window = 0;
-        
+        public static int window = 0;
+
         public static Start w_Start;
         public static Test_choice w_Test_Choice;
         public static Test_Start w_Test_Start;
@@ -197,8 +208,8 @@ namespace PsychoAT
         static void Main()
         {
             db.load_all_tests();
-            db.load_current_test(4);
-            
+            db.load_current_test(1);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             w_Start = new Start();
@@ -207,7 +218,7 @@ namespace PsychoAT
             w_Test_Start = new Test_Start();
             w_Result = new Result();
             w_Stat = new Statistics();
-            
+
 
             Application.Run(w_Start);
 
